a0ef6f4020490b3221d9e9c29f1293eee1159f5d
Adam Nuccio <stnt190@protonmail.com>
2020-10-16 22:33:58 -0400

    Add files via upload

---
+++ DausBot/Modules/commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DausBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Ping")]
+        public async Task Ping() => await ReplyAsync("Your Internet Blows");
+    }
+}
+
+
